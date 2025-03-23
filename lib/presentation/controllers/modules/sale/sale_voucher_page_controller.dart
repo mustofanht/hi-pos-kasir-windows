91ecfb659ee253a09a395602f04493f071b4a991
Sarip Hidayatullah <102409123+sya17@users.noreply.github.com>
2025-03-23 22:42:05 +0700

    voucher
    filter inq voucher

--- lib/presentation/controllers/modules/sale/sale_voucher_page_controller.dart
+++ lib/presentation/controllers/modules/sale/sale_voucher_page_controller.dart
@@ -200,37 +200,37 @@ class SaleVoucherPageController extends GetxController {
         )!,
       );
 
-      // List<FilterQuery> dataFilterDate = [];
+      List<FilterQuery> dataFilterDate = [];
 
-      // dataFilterDate.add(
-      //   apiFilterUtil.addSearch(
-      //     'vpEndDate',
-      //     OPERATOR_CONSTANTS.GREATHER_THAN_OR_EQUALS,
-      //     dateTimeUtil.dateFormat(DateTime.now(), 'yyyy-MM-dd'),
-      //   )!,
-      // );
-      // dataFilterDate.add(
-      //   apiFilterUtil.addSearch(
-      //     'vpEndDate',
-      //     OPERATOR_CONSTANTS.EQUALS,
-      //     OPERATOR_CONSTANTS.IS_NULL,
-      //   )!,
-      // );
+      dataFilterDate.add(
+        apiFilterUtil.addSearch(
+          'vpEndDate',
+          OPERATOR_CONSTANTS.GREATHER_THAN_OR_EQUALS,
+          '${dateTimeUtil.dateFormat(DateTime.now(), 'yyyy-MM-dd')}:OR',
+        )!,
+      );
+      dataFilterDate.add(
+        apiFilterUtil.addSearch(
+          'vpEndDate',
+          OPERATOR_CONSTANTS.EQUALS,
+          '${OPERATOR_CONSTANTS.IS_NULL}:OR',
+        )!,
+      );
 
-      // dataFilter.add(
-      //   apiFilterUtil.addSearch(
-      //     'GROUP_CONDITION_OR',
-      //     OPERATOR_CONSTANTS.EQUALS,
-      //     dataFilterDate,
-      //   )!,
-      // );
-      // dataFilter.add(
-      //   apiFilterUtil.addSearch(
-      //     'vpStartDate',
-      //     OPERATOR_CONSTANTS.LESS_THAN_OR_EQUALS,
-      //     dateTimeUtil.dateFormat(DateTime.now(), 'yyyy-MM-dd'),
-      //   )!,
-      // );
+      dataFilter.add(
+        apiFilterUtil.addSearch(
+          'GROUP_CONDITION_AND',
+          OPERATOR_CONSTANTS.EQUALS,
+          dataFilterDate,
+        )!,
+      );
+      dataFilter.add(
+        apiFilterUtil.addSearch(
+          'vpStartDate',
+          OPERATOR_CONSTANTS.LESS_THAN_OR_EQUALS,
+          dateTimeUtil.dateFormat(DateTime.now(), 'yyyy-MM-dd'),
+        )!,
+      );
 
       result = await _service.sale.voucherService.getAllVoucher(
         authToken: _authToken,
